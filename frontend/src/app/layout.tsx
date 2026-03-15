7628c63f1bb6a7e37f7b536daef565deeed1966c
4oeuhtns <zhaofelixdelt@gmail.com>
2026-03-15 05:38:07 -0400

    changes to font and homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,25 +1,26 @@
 import type { Metadata } from "next";
-import { Inter, Fira_Code } from "next/font/google";
+import { Instrument_Sans, Fira_Code, Instrument_Serif } from "next/font/google";
 import localFont from "next/font/local";
 import { Toaster } from "@/components/ui/sonner";
 import { Providers } from "@/components/providers";
 import "./globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
+const instrumentSans = Instrument_Sans({
+  variable: "--font-instrument-sans",
   subsets: ["latin"],
 });
 
 const firaCode = Fira_Code({
+  weight: "400",
   variable: "--font-fira-code",
   subsets: ["latin"],
 });
 
-const junicode = localFont({
-  src: "../../public/fonts/Junicode-BoldItalic.ttf",
-  variable: "--font-junicode",
-  weight: "700",
+const instrumentSerif = Instrument_Serif({
+  weight: "400",
   style: "italic",
+  variable: "--font-instrument-serif",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -33,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`dark ${inter.variable} ${firaCode.variable} ${junicode.variable}`}>
+    <html lang="en" className={`dark ${instrumentSans.variable} ${firaCode.variable} ${instrumentSerif.variable}`}>
       <body className="antialiased bg-dark text-light">
 
         <Providers>
